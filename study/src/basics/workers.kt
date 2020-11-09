b74a0f553111f8bcaf7c94c0d6d7bdf3850086b1
Elena Popova <fogelvogel1337@gmail.com>
2020-11-09 20:02:29 +0700

    Модуль 2, задание 11

---
+++ study/src/basics/workers.kt
@@ -0,0 +1,32 @@
+package basics
+
+fun main(args: Array<String>){
+    println("Справочник работников!")
+    val workersSeats:  MutableMap<Int, String> = mutableMapOf()
+    while (true) {
+        println("нажмите 1 чтобы добавить сотрудника")
+        println("нажмите 2 чтобы посмотреть список сотрудников")
+        println("нажмите 3 чтобы найти сотрудника по номеру места")
+        println("нажмите 4 чтобы выйти из программы")
+        val currentChoice = readLine()!!
+        when (currentChoice) {
+            "1" -> {
+                println("Введите фамилию сотрудника")
+                val worker = readLine()!!
+                println("Введите номер рабочего места сотрудника")
+                val seat = readLine()!!.toInt()
+                workersSeats.put(seat, worker);
+            }
+            "2" -> for (i in workersSeats) {
+                println(i)
+            }
+            "3" -> {
+                println("Введите номер рабочего места сотрудника")
+                val seat = readLine()!!.toInt()
+                println(workersSeats.get(seat))
+            }
+
+            "4" -> break
+        }
+    }
+}
